0cddf37e1b229b522305e87da231c7b9ec51788e
StevenPerdomo <78056724+StevenPerdomo@users.noreply.github.com>
2021-02-26 00:18:35 -0600

    Creacion de Clase Contexto

---
+++ FacturacionSB/BLSeguridad/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLFacturacionSB
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base()
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}

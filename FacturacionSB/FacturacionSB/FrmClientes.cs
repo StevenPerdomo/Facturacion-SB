d90229f58d210aada4c5f49ce0f082960c44aa5e
StevenPerdomo <78056724+StevenPerdomo@users.noreply.github.com>
2021-02-25 23:29:34 -0600

    Configuracion de Boton Eliminar

--- FacturacionSB/FacturacionSB/FrmClientes.cs
+++ FacturacionSB/FacturacionSB/FrmClientes.cs
@@ -146,8 +146,13 @@ namespace FacturacionSB
         {
             if (idTextBox.Text != "")
             {
-                var id = Convert.ToInt32(idTextBox.Text);
-                Eliminar(id);
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if(resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }
+                
             }
         }
 
